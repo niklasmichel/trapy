f2e7e40c57734bac821b0866d3e05f8401bf9320
niklasmichel <niklas.michel@gmail.com>
2020-06-04 11:34:49 +0200

    organised trial metrics and time courses into TRA.metrics and TRA.data

--- tests/test.py
+++ tests/test.py
@@ -3,13 +3,4 @@ from pathlib import Path
 
 folder = str(Path.cwd()) + '/test_data'
 experiment = TRA(folder)
-experiment.to_excel()
-
-# /home/niklas/anaconda3/bin/python /home/niklas/PycharmProjects/trapy/tests/test.py
-# Calculated Metrics from txt files in /home/niklas/PycharmProjects/trapy/tests/test_data
-# Analyzing txt files in given folder...
-# Writing sheet "All Trials Metrics" to /home/niklas/PycharmProjects/trapy/tests/test_data/TapeResponseAssay.xlsx.
-# Writing sheet for time courses of group "group_1" to /home/niklas/PycharmProjects/trapy/tests/test_data/TapeResponseAssay.xlsx.
-# Writing sheet for time courses of group "group_2" to /home/niklas/PycharmProjects/trapy/tests/test_data/TapeResponseAssay.xlsx.
-# All done; see /home/niklas/PycharmProjects/trapy/tests/test_data/TapeResponseAssay.xlsx
-# Process finished with exit code 0
+print(experiment.data)
--- trapy/tra.py
+++ trapy/tra.py
@@ -33,6 +33,8 @@ class TRA:
         .groups     - set of experimental groups
         .dates      - set of dates of trials
         .mice       - set of mouse numbers
+        .metrics    - DataFrame of metrics per trial
+        .data       - Dictionary of DataFrames of bout-time-data per group
     Methods:
         .to_excel() - writes metrics and time courses to
                       'TapeResponseAssay.xlsx'
@@ -44,8 +46,22 @@ class TRA:
         self.groups = set(trial.group for trial in self.trials)
         self.dates = set(trial.date for trial in self.trials)
         self.mice = set(trial.mouse for trial in self.trials)
+        self.metrics = trials_to_df(self.trials)
+        self.data = timecourses_to_dfs(self)
 
 
+    def plot_data(self):
+        # plt array of 2x(nr of groups) subfigures:
+        # - left: single bout-time-curves per group
+        # - right: group average +- SD or SEM, maybe specify as argument/param
+        # - bottom row: left all trials, right all group averages
+        pass
+
+    def plot_results(self):
+        # plt array of (bar graphs?)
+        # - total bouts means | total time means | bpm means
+        # - success rates | idle time means | auc means
+        pass
 
     def to_excel(self):
         """Crawls folder for txt files; instantiates them as Trial() objects,
@@ -53,64 +69,71 @@ class TRA:
         prints these metrics to 'TapeResponseAssay.xlsx'.
         Creates worksheets for all metrics and timecourse per group, respectively.
         """
-        sys.stdout.write('Analyzing txt files in given folder...\n')
-        # trials = instantiate(folder)
-        # groups = {trial.group for trial in trials}
         outputfile = os.path.join(self.folder, 'TapeResponseAssay.xlsx')
         writer = pd.ExcelWriter(outputfile, engine='xlsxwriter')
 
-        # Write each dataframe (all metrics, group metrics, group timecourse) to a different worksheet.
+        # Write each dataframe (all metrics, group timecourse) to a different worksheet.
 
-        df_metrics_all = pd.DataFrame({
-            'Group': [trial.group for trial in self.trials],
-            'Mouse': [trial.mouse for trial in self.trials],
-            'Date': [trial.date for trial in self.trials],
-            'Success': [trial.success for trial in self.trials],
-            'Total bouts': [trial.total_bouts for trial in self.trials],
-            'Total time': [trial.total_time for trial in self.trials],
-            'Idle time': [trial.idle_time for trial in self.trials],
-            'Bouts per minute': [trial.bouts_per_minute for trial in self.trials],
-            # 'TIBI (touch induced behaviour index, BPM * (5 minutes - idle time))': [trial.tibi for trial in trials],
-            'AUC (Area under the trial time course curve)': [trial.auc for trial in self.trials]
-        })
-        df_metrics_all.to_excel(writer, sheet_name='All Trials Metrics', index=False)
-        sys.stdout.write(f'Writing sheet "All Trials Metrics" to {outputfile}.\n')
+        df_metrics = self.metrics
+        df_metrics.to_excel(writer, sheet_name='All Trials Metrics', index=False)
+        sys.stdout.write(f'Writing sheet "All Trials Metrics" to excel file.\n')
 
         for group in self.groups:
-            # df_metrics = pd.DataFrame({
-            #     'Group': [trial.group for trial in self.trials if trial.group == group],
-            #     'Mouse': [trial.mouse for trial in self.trials if trial.group == group],
-            #     'Date': [trial.date for trial in self.trials if trial.group == group],
-            #     'Success': [trial.success for trial in self.trials if trial.group == group],
-            #     'Total bouts': [trial.total_bouts for trial in self.trials if trial.group == group],
-            #     'Total time': [trial.total_time for trial in self.trials if trial.group == group],
-            #     'Idle time': [trial.idle_time for trial in self.trials if trial.group == group],
-            #     'Bouts per minute': [trial.bouts_per_minute for trial in self.trials if trial.group == group],
-            #     # 'TIBI (touch induced behaviour index, BPM * (5 minutes - idle time))': [trial.tibi for trial in trials if
-            #     #                                                                        trial.group == group],
-            #     'AUC (Area under the trial time course curve)': [trial.auc for trial in self.trials if
-            #                                                      trial.group == group]
-            # })
-            # df_metrics.to_excel(writer, sheet_name=str(group) + '_Metrics', index=False)
-            # sys.stdout.write('Writing sheet for metrics of group "{0}" to {1}.\n'.format(group, outputfile))
-
-            df_timecourse = pd.DataFrame({trial.mouse: trial.timecourse300 for trial in self.trials
-                                          if trial.group == group})
-            df_timecourse['Mean'] = df_timecourse[[trial.mouse for trial in self.trials
-                                                   if trial.group == group]].mean(axis=1)
-            df_timecourse['SD(n-1)'] = df_timecourse[[trial.mouse for trial in self.trials
-                                                      if trial.group == group]].std(axis=1, ddof=1)
-            df_timecourse['n'] = df_timecourse[[trial.mouse for trial in self.trials
-                                                if trial.group == group]].count(axis=1)
-            cols = df_timecourse.columns.tolist()
-            cols = cols[-3:] + cols[:-3]
-            df_timecourse = df_timecourse[cols]
+            df_timecourse = self.data[group]
             df_timecourse.to_excel(writer,
                                    sheet_name=str(group) + '_Time courses',
                                    index=True)
             sys.stdout.write(
-                f'Writing sheet for time courses of group "{group}" to {outputfile}.\n')
+                f'Writing sheet for time courses of group "{group}" to excel file.\n')
 
-        # Close the Pandas Excel writer and output the Excel file.
         writer.save()
         sys.stdout.write(f'All done; see {outputfile}')
+
+
+def trials_to_df(trials):
+    """Returns DataFrame of trial metrics."""
+    group, mouse, date, success, total_bouts, total_time, idle_time, bpm, auc = [], [], [], [], [], [], [], [], []
+
+    for trial in trials:
+        group.append(trial.group)
+        mouse.append(trial.mouse)
+        date.append(trial.date)
+        success.append(trial.success)
+        total_bouts.append(trial.total_bouts)
+        total_time.append(trial.total_time)
+        idle_time.append(trial.idle_time)
+        bpm.append(trial.bouts_per_minute)
+        auc.append(trial.auc)
+
+    df_metrics = pd.DataFrame({
+        'Group': group,
+        'Mouse': mouse,
+        'Date': date,
+        'Success': success,
+        'Total bouts': total_bouts,
+        'Total time': total_time,
+        'Idle time': idle_time,
+        'Bouts per minute': bpm,
+        'AUC (Area under the trial time course curve)': auc
+    })
+
+    return df_metrics
+
+
+def timecourses_to_dfs(experiment):
+    tc_groups = dict()
+    for group in experiment.groups:
+        df_timecourse = pd.DataFrame({trial.mouse: trial.timecourse300 for trial in experiment.trials
+                                      if trial.group == group})
+        df_timecourse['Mean'] = df_timecourse[[trial.mouse for trial in experiment.trials
+                                               if trial.group == group]].mean(axis=1)
+        df_timecourse['SD(n-1)'] = df_timecourse[[trial.mouse for trial in experiment.trials
+                                                  if trial.group == group]].std(axis=1, ddof=1)
+        df_timecourse['n'] = df_timecourse[[trial.mouse for trial in experiment.trials
+                                            if trial.group == group]].count(axis=1)
+        cols = df_timecourse.columns.tolist()
+        cols = cols[-3:] + cols[:-3]
+        df_timecourse = df_timecourse[cols]
+        tc_groups[group] = df_timecourse
+
+    return tc_groups
